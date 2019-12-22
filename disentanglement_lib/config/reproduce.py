5f0eaf7de7d36528e92dc694abce14fa25bd6116
ftraeuble <ftraeuble@outlook.com>
2019-12-22 17:34:36 +0100

    added correlation_beta study

--- disentanglement_lib/config/reproduce.py
+++ disentanglement_lib/config/reproduce.py
@@ -24,9 +24,11 @@ from disentanglement_lib.config.tests import sweep as tests
 from disentanglement_lib.config.unsupervised_study_v1 import sweep as unsupervised_study_v1
 from disentanglement_lib.config.correlated_factors_study_v1 import sweep as correlated_factors_study_v1
 from disentanglement_lib.config.correlated_factors_study_dry_run import sweep as correlated_factors_study_dry_run
+from disentanglement_lib.config.correlated_factors_study_beta import sweep as correlated_factors_study_beta
 
 STUDIES = {
     "correlated_factors_study_dry_run": correlated_factors_study_dry_run.CorrelatedFactorsStudyDryRun(),
+    "correlated_factors_study_beta": correlated_factors_study_beta.CorrelatedFactorsStudyBeta(),
     "correlated_factors_study_v1": correlated_factors_study_v1.CorrelatedFactorsStudyV1(),
     "unsupervised_study_v1": unsupervised_study_v1.UnsupervisedStudyV1(),
     "abstract_reasoning_study_v1":

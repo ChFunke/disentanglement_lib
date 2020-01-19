78b547f88ee0a18b5d8e500854f3c284c6d959b2
ftraeuble <ftraeuble@outlook.com>
2020-01-20 00:57:11 +0100

    added new metrics

--- disentanglement_lib/evaluation/evaluate.py
+++ disentanglement_lib/evaluation/evaluate.py
@@ -25,6 +25,7 @@ from disentanglement_lib.data.ground_truth import named_data
 from disentanglement_lib.evaluation.metrics import beta_vae  # pylint: disable=unused-import
 from disentanglement_lib.evaluation.metrics import dci  # pylint: disable=unused-import
 from disentanglement_lib.evaluation.metrics import dci_afa  # pylint: disable=unused-import
+from disentanglement_lib.evaluation.metrics import entanglement_metrics_afa  # pylint: disable=unused-import
 from disentanglement_lib.evaluation.metrics import fairness_afa  # pylint: disable=unused-import
 from disentanglement_lib.evaluation.metrics import fairness_afa_partial  # pylint: disable=unused-import
 from disentanglement_lib.evaluation.metrics import dci_afa_partial  # pylint: disable=unused-import
